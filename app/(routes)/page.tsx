b3f472fc03f4412a14183da97c75c48e25126b58
MahdiArabpour <85918072+MahdiArabpour98@users.noreply.github.com>
2023-10-28 08:54:42 +0330

    protected pages

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,27 @@
-import Image from "next/image";
+import { signOut, getSession } from "next-auth/react";
+import { NextPageContext } from "next";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {},
+  };
+}
 
 export default function Home() {
-  return <div className="text-2xl text-green-500">netflix app</div>;
+  return (
+    <div className="text-2xl text-green-500">
+      <button className="w-full h-10 bg-white" onClick={() => signOut()}>
+        logout
+      </button>
+    </div>
+  );
 }
